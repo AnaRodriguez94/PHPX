ac564d8a54b340b3e04da7d885e7fbb5b3ba6bf7
Joaquin Ortega Blanco <joaquinortegablanco@MacBook-Air-de-Joaquin.local>
2024-06-29 11:09:17 +0200

    Muestro un listado de tweets

--- index.php
+++ index.php
@@ -4,5 +4,23 @@ $text = "Soy un coche rojo";
 $author = "Cervantes";
 $user = "Ana";
 
+$tweets = [
+    [
+        'text' => $text,
+        'author' => $author,
+        'user' => $user
+    ],
+    [
+        'text' => 'Hola buenos dias',
+        'author' => 'Moises',
+        'user' => 'Moises123'
+    ],
+    [
+        'text' => 'En un lugar de la mancha',
+        'author' => 'Cervantes',
+        'user' => 'MACervantes'
+    ]
+];
+
 require_once "index.html";
 
